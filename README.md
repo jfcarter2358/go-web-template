ee8bd39f144ad72b7a18f46ef3bf88477b531d37
John Carter <jfcarter2358@gmail.com>
2024-09-25 15:33:22 -0500

    ENH: Fix a few references still to Scaffold, fix setup.sh

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 
 ## Contact
 
+This software is written and maintained by ${AUTHOR_NAME}, if you have any questions or issues please don't hesitate to reach out at ${AUTHOR_EMAIL}
--- setup.sh
+++ setup.sh
@@ -21,5 +21,15 @@ echo -n "Docker org: "
 read -r DOCKER_ORG
 
 echo "Updating stub files..."
-sed -i '' "s/\${APP_NAME}/${APP_NAME}/g" $(git ls-files | grep -v setup.sh)
-sed -i '' "s/\${APP_NAME}/${APP_NAME}/g" $(git ls-files | grep -v setup.sh)
+sed -i "s/\${APP_NAME}/${APP_NAME}/g" $(git ls-files | grep -v setup.sh)
+sed -i "s/\${APP_NAME_CAPITAL}/${APP_NAME_CAPITAL}/g" $(git ls-files | grep -v setup.sh)
+sed -i "s/\${APP_NAME_UPPERCASE}/${APP_NAME_UPPERCASE}/g" $(git ls-files | grep -v setup.sh)
+sed -i "s/\${APP_DESCRIPTION}/${APP_DESCRIPTION}/g" $(git ls-files | grep -v setup.sh)
+sed -i "s/\${APP_PORT}/${APP_PORT}/g" $(git ls-files | grep -v setup.sh)
+sed -i "s/\${AUTHOR_NAME}/${AUTHOR_NAME}/g" $(git ls-files | grep -v setup.sh)
+sed -i "s/\${AUTHOR_EMAIL}/${AUTHOR_EMAIL}/g" $(git ls-files | grep -v setup.sh)
+sed -i "s/\${DOCKER_ORG}/${DOCKER_ORG}/g" $(git ls-files | grep -v setup.sh)
+
+echo "Done!"
+
+rm setup.sh
